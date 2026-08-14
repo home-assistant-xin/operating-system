cefd6b2c8f9230908db0def4a41b18533f807fbb
ydshk <10000@tiyicn.cn>
2026-08-14 23:12:11 +0800

    初始化

--- tests/smoke_test/test_os_update.py
+++ tests/smoke_test/test_os_update.py
@@ -51,7 +51,7 @@ def test_os_update(shell, shell_json, target):
     shell.run_check("ha su reload --no-progress")
 
     # update OS to latest stable - in tests it should never be the same version
-    stable_version = shell_json("curl -sSL https://version.home-assistant.io/stable.json")["hassos"]["ova"]
+    stable_version = shell_json("curl -sSL https://version.home-assistant.xin/stable.json")["hassos"]["ova"]
 
     # Core (and maybe Supervisor) might be downloaded at this point, so we need to keep trying
     while True:
